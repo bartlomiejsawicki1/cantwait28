3b85dbe80441890f56ad0a89a5d413a700bac815
bartlomiejsawicki1 <barteksaw20@wp.pl>
2023-05-20 15:55:49 +0200

    Implement user profile page

--- lib/features/auth/auth_gate.dart
+++ lib/features/auth/auth_gate.dart
@@ -1,32 +1,27 @@
 import 'package:cantwait28/features/home/pages/home_page.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final providers = [EmailAuthProvider()];
-
-    return MaterialApp(
-      initialRoute:
-          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
-      routes: {
-        '/sign-in': (context) {
+    return StreamBuilder<auth.User?>(
+      stream: auth.FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
           return SignInScreen(
-            providers: providers,
-            actions: [
-              AuthStateChangeAction<SignedIn>((context, state) {
-                Navigator.pushReplacementNamed(context, '/profile');
-              }),
+            providers: [
+              EmailAuthProvider(),
             ],
           );
-        },
-        '/profile': (context) {
-          return const HomePage();
-        },
+        }
+
+        // Render your application if authenticated
+        return const HomePage();
       },
     );
   }
